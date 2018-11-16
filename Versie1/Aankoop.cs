ded4486ea1e29e4b7f7bfc1a250fe4f148ce9d09
Toon Meyvaert <toon.meyvaert@gmail.com>
2018-11-16 14:14:03 +0100

    Add project files.

---
+++ Versie1/Aankoop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versie1
+{
+    class Aankoop
+    {
+        private double dblPrijs;
+        private int intIdAankoop, intAantal;
+
+        public int idAankoop
+        {
+            get { return intIdAankoop; }
+            set { intIdAankoop = value; }
+        }
+        public double prijs
+        {
+            get { return dblPrijs; }
+            set { dblPrijs = value; }
+        }
+        public int aantal
+        {
+            get { return intAantal; }
+            set { intAantal = value; }
+        }
+
+        public Aankoop(int pintIdAankoop, double pdblPrijs, int pintAantal)
+        {
+            intIdAankoop = pintIdAankoop;
+            dblPrijs = pdblPrijs;
+            intAantal = pintAantal;
+        }
+
+
+    }
+}
---
+++ Versie1/Persistence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Versie1
+{
+    class Persistence
+    {
+        private string _connectionstring;
+
+        public Persistence()
+        {
+            _connectionstring = "server=localhost;user id=root;Password='Test123';database=database";
+        }
+
+        public List<Aankoop> getEvaluatieFromDB()
+        {
+            List<Aankoop> lijst = new List<Aankoop>();
+
+            MySqlConnection conn = new MySqlConnection(_connectionstring);
+            MySqlCommand cmd = new MySqlCommand("select * from aankoop", conn);
+
+            conn.Open();
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Aankoop eval = new Aankoop(Convert.ToInt32(dataReader["idAankoop"]), Convert.ToDouble(dataReader["Prijs"]), Convert.ToInt32(dataReader["Aantal"]));
+                lijst.Add(eval);
+            }
+
+            conn.Close(); 
+            return lijst; 
+        }
+    }
+}
