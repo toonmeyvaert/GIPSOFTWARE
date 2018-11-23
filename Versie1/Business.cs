3237d6dbbf04b8cba8ceabfa4be70eda9e0fd6eb
Jens Reynvoet <reynvoet.jens@gmail.com>
2018-11-23 15:49:42 +0100

    Commitkoning

--- Versie1/Business.cs
+++ Versie1/Business.cs
@@ -8,6 +8,34 @@ namespace Versie1
 {
     class Business
     {
+        //instantie van persistence
+        private Persistence _persistence;
+
+        private List<Aankoop> _aankoop;
+        public List<Aankoop> aankoop
+        {
+            get { return _aankoop; }
+            set { _aankoop = value; }
+        }
+
+        //constructor
+        public Business()
+        {
+            _persistence = new Persistence();
+            //lijst opvullen met data
+            _aankoop = _persistence.getAankoopFromDB();
+        }
+
+        public List<string> getAankopen()
+        {
+            List<string> result = new List<string>();
+            foreach (Aankoop item in _aankoop)
+            {
+                result.Add(item.ToString());
+            }
+            return result;
+        }
+
 
     }
 }
